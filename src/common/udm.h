c23081156e5d22176137621198d79852df0528cb
Lukas Rotheneder <lukas.rotheneder@phine.tech>
2024-01-09 14:10:10 +0100

    fix(udm): remove nrf dependency

--- src/common/udm.h
+++ src/common/udm.h
@@ -23,6 +23,7 @@
 #define FILE__SEEN
 
 #define HEART_BEAT_TIMER 10
+#define NRF_REGISTRATION_RETRY_TIMER 5
 
 #define _unused(x) ((void) (x))
 
--- src/udm_app/udm_nrf.hpp
+++ src/udm_app/udm_nrf.hpp
@@ -55,18 +55,42 @@ class udm_nrf {
   void operator=(udm_nrf const&) = delete;
 
   void generate_uuid();
+
   /*
    * Start event nf heartbeat procedure
    * @param [void]
    * @return void
    */
   void start_event_nf_heartbeat(std::string& remoteURI);
+
   /*
    * Trigger NF heartbeat procedure
    * @param [void]
    * @return void
    */
   void trigger_nf_heartbeat_procedure(uint64_t ms);
+
+  /*
+   * Start event nrf registration retry
+   * @param [void]
+   * @return void
+   */
+  void start_nrf_registration_retry();
+
+  /*
+   * Trigger NF registration procedure
+   * @param [void]
+   * @return void
+   */
+  void trigger_nrf_registration_retry_procedure(uint64_t ms);
+
+  /*
+   * Stop event nrf registration retry
+   * @param [void]
+   * @return void
+   */
+  void stop_nrf_registration_retry();
+
   /*
    * Generate a UDM profile for this instance
    * @param [void]
@@ -81,6 +105,7 @@ class udm_nrf {
    * @return void
    */
   void register_to_nrf();
+
   /*
    * Get udm API Root
    * @param [std::string& ] api_root: udm's API Root
@@ -91,6 +116,7 @@ class udm_nrf {
  private:
   udm_event& m_event_sub;
   bs2::connection task_connection;
+  bs2::connection retry_nrf_registration_task_connection;
 };
 }  // namespace app
 }  // namespace udm
